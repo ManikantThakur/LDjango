14e4593bbc8e110b9f247b0d0c00697db1fcd7b2
Manikant Thakur <info.manikant@gmail.com>
2016-01-18 17:53:04 +0530

    Django App2 done

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return HttpResponse('Hello, we are in index.')
+
+
+def details(request, question_id):
+    return HttpResponse("You are looking details of question# %s" % question_id)
+
+
+def results(request, question_id):
+    response = "You are looking the result of question# %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question# %s" % question_id)
+
